7ba6b2cee0f2df8f13a4ebf916a51719ba367536
RicardoMoya <setmoya@gmail.com>
2016-02-14 19:12:54 +0100

    Primera versión del método K-Means para 2 data set de datos al que hay que aplicar 3 clusters

---
+++ KMeans/Point.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'RicardoMoya'
+
+
+class Point:
+    '''
+    Class to represent a point in N dimension
+    '''
+
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.dimension = len(coordinates)
+
+    def __repr__(self):
+        return 'Coordinates: ' + str(self.coordinates) + ' -> Dimension: ' + str(self.dimension)
