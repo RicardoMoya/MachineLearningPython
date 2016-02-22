d4aad4f9facc5e760ed20444eda9b56bed83d89a
Ricardo Moya <rmoya@logtrust.com>
2016-02-22 21:06:14 +0100

    solucionado el error de generaci´ón del  dataset de 7 clusters

--- dataSet/DSclustering/genDataSetClustering.py
+++ dataSet/DSclustering/genDataSetClustering.py
@@ -3,12 +3,12 @@ __author__ = 'RicardoMoya'
 
 from sklearn.datasets.samples_generator import make_blobs
 
-THEORETICAL_CENTROIDS = ((-1,3),(0,0),(0,6),(2,3),(4,5),(5,2),(7,5))
+THEORETICAL_CENTROIDS = [[-1,3],[0,0],[0,6],[2,3],[4,5],[5,2],[7,5]]
 NUM_POINTS = 100000
 
 points, labels = make_blobs(n_samples=NUM_POINTS,
                             n_features=len(THEORETICAL_CENTROIDS[0]),
-                            centers=len(THEORETICAL_CENTROIDS),
+                            centers=THEORETICAL_CENTROIDS,
                             cluster_std=0.7)
 
 file = './DS_' + str(len(THEORETICAL_CENTROIDS)) + 'Clusters_' + str(
