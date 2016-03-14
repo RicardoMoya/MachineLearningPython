df1bcbb7e6a470bbce3b284cd98d52d6f2aee323
Ricardo Moya <rmoya@logtrust.com>
2016-03-14 20:14:47 +0100

    separacion de proyectos y organizaci´ón en submodulos

---
+++ dataSet/DSclustering/GenDataSetClustering.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__author__ = 'RicardoMoya'
+
+from sklearn.datasets.samples_generator import make_blobs
+
+THEORETICAL_CENTROIDS = [[-1,3],[0,0],[0,6],[2,3],[4,5],[5,2],[7,5]]
+NUM_POINTS = 100000
+
+points, labels = make_blobs(n_samples=NUM_POINTS,
+                            n_features=len(THEORETICAL_CENTROIDS[0]),
+                            centers=THEORETICAL_CENTROIDS,
+                            cluster_std=0.7)
+
+file = './DS_' + str(len(THEORETICAL_CENTROIDS)) + 'Clusters_' + str(
+    NUM_POINTS) + 'Points.txt'
+fichero = open(file, 'w')
+for p in points:
+    fichero.write('::'.join(['%f' % num for num in p]) + '\n')
+fichero.close()
