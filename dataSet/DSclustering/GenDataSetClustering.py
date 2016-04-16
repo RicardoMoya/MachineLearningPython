8f867be873592bab89ca560ea1ade1c8773501db
Ricardo Moya <rmoya@logtrust.com>
2016-04-16 12:08:39 +0200

    cambios varios

--- dataSet/DSclustering/GenDataSetClustering.py
+++ dataSet/DSclustering/GenDataSetClustering.py
@@ -3,7 +3,8 @@ __author__ = 'RicardoMoya'
 
 from sklearn.datasets.samples_generator import make_blobs
 
-THEORETICAL_CENTROIDS = [[-1,3],[0,0],[0,6],[2,3],[4,5],[5,2],[7,5]]
+THEORETICAL_CENTROIDS = [[-1, 3], [0, 0], [0, 6], [2, 3], [4, 5], [5, 2],
+                         [7, 5]]
 NUM_POINTS = 100000
 
 points, labels = make_blobs(n_samples=NUM_POINTS,
