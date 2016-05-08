fc0341b8e22e4c642732b947ca0e74e5befedb9d
RicardoMoya <RicardoMoya>
2016-05-08 13:01:28 +0200

    code style

--- dataSet/DSclustering/GenDataSetClustering.py
+++ dataSet/DSclustering/GenDataSetClustering.py
@@ -1,9 +1,10 @@
+from sklearn.datasets.samples_generator import make_blobs
+
 # -*- coding: utf-8 -*-
 __author__ = 'RicardoMoya'
 
-from sklearn.datasets.samples_generator import make_blobs
-
-THEORETICAL_CENTROIDS = [[-1,3],[0,0],[0,6],[2,3],[4,5],[5,2],[7,5]]
+THEORETICAL_CENTROIDS = [[-1, 3], [0, 0], [0, 6], [2, 3], [4, 5], [5, 2],
+                         [7, 5]]
 NUM_POINTS = 100000
 
 points, labels = make_blobs(n_samples=NUM_POINTS,
@@ -11,9 +12,9 @@ points, labels = make_blobs(n_samples=NUM_POINTS,
                             centers=THEORETICAL_CENTROIDS,
                             cluster_std=0.7)
 
-file = './DS_' + str(len(THEORETICAL_CENTROIDS)) + 'Clusters_' + str(
+file_dataset = './DS_' + str(len(THEORETICAL_CENTROIDS)) + 'Clusters_' + str(
     NUM_POINTS) + 'Points.txt'
-fichero = open(file, 'w')
+fichero = open(file_dataset, 'w')
 for p in points:
     fichero.write('::'.join(['%f' % num for num in p]) + '\n')
 fichero.close()
